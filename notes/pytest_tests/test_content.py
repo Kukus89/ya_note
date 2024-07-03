293d60a13b78ff6915f022f1dbeeac45d372565b
Anton B. <Kukus89@gmail.com>
2024-07-03 16:00:00 +0300

    pytest

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -1,10 +1,39 @@
-# from django.test import TestCase
-# from notes.models import Note
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
-# from notes.forms import NoteForm
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+import pytest
+from notes.forms import NoteForm
+User = get_user_model()
 
-# User = get_user_model()
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('not_author_client'), False),
+    )
+)
+def test_notes_list_for_different_users(
+        note, parametrized_client, note_in_list
+):
+    url = reverse('notes:list')
+    response = parametrized_client.get(url)
+    assert (note in response.context['object_list']) is note_in_list
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:add', None),
+        ('notes:edit', pytest.lazy_fixture('note_slug'))
+    )
+)
+def test_pages_contained_form(
+        name, args, author_client
+):
+    url = reverse(name, args=args)
+    response = author_client.get(url)
+    assert "form" in response.context
+    assert isinstance(response.context['form'], NoteForm)
 
 
 # class TestContent(TestCase):
--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,11 +1,68 @@
-# from django.test import Client, TestCase
-# from http import HTTPStatus
+import pytest
+from http import HTTPStatus
 from django.urls import reverse
-# from notes.models import Note
-# from django.contrib.auth import get_user_model
+from pytest_django.asserts import assertRedirects
 
 
-# User = get_user_model()
+@pytest.mark.parametrize(
+    "name",
+    ("notes:home", "users:login", "users:logout", "users:signup")
+)
+def test_home_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete')
+)
+def test_pages_availability_for_users(
+        parametrized_client,
+        expected_status,
+        name,
+        note
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture("note_slug")),
+        ('notes:edit', pytest.lazy_fixture("note_slug")),
+        ('notes:delete', pytest.lazy_fixture("note_slug")),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    url = reverse(name, args=(args))
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
 
 
 # class TestRoutes(TestCase):
@@ -67,14 +124,3 @@ from django.urls import reverse
 #                 redirect_url = f'{login_url}?next={url}'
 #                 response = self.client.get(url)
 #                 self.assertRedirects(response, redirect_url)
-
-
-def test_with_client(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
-
-def test_closed_page(admin_client):
-    url = reverse('notes:list')
-    response = admin_client.get(url)
-    assert response.status_code == 200
